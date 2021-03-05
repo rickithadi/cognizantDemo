dd0749af8ffad2b2c2de8a3ce39a0213bea48d18
hadi <rickithadi@gmail.com>
2021-03-06 00:45:31 +0800

    added bootstrap css and gave the thing a header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Header from './components/Header'
 import { Container } from "react-bootstrap";
 import axios from "axios";
 function App() {
@@ -19,35 +20,35 @@ function App() {
                 .all([
                     axios.get(
                         "https://api.data.gov.sg/v1/transport/traffic-images?date_time=" +
-                            queryString
+                        queryString
                     ),
                     axios.get(
                         "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time=" +
-                            queryString
+                        queryString
                     ),
                 ])
                 .then(
                     axios.spread((response1, response2) => {
                         setCameraData(response1.data.items[0]); //object
 
-                        let areaNforecastArray = [];
-                        const forecast = response2.data.items[0].forecasts; //[{area:"AMK", forecast="Fair"}, {},{}]
-                        const area_metadata = response2.data.area_metadata; //[{name:"AMK", label_location:{}}]
-                        // Loop the area_metadata, for each {} in array, filter forecast [{}] with matching name, combined forecast with area_metadata
-                        for (let i = 0; i < area_metadata.length; i++) {
-                            //filter the {} with matching area
-                            let correspondingForecast = forecast.filter(
-                                (forecastData) =>
-                                    forecastData.area === area_metadata[i].name
-                            ); //[{area:"AMK", forcast:"windy"}]
-                            //have a combined {} of name, label_location, forecast
-                            let areaNforecast = {
-                                ...area_metadata[i],
-                                forecast: correspondingForecast[0].forecast,
-                            };
-                            areaNforecastArray.push(areaNforecast);
-                        }
-                        setWeatherData(areaNforecastArray);
+                        //     let areaNforecastArray: any = [];
+                        //     const forecast = response2.data.items[0].forecasts; //[{area:"AMK", forecast="Fair"}, {},{}]
+                        //     const area_metadata = response2.data.area_metadata; //[{name:"AMK", label_location:{}}]
+                        //     // Loop the area_metadata, for each {} in array, filter forecast [{}] with matching name, combined forecast with area_metadata
+                        //     for (let i = 0; i < area_metadata.length; i++) {
+                        //         //filter the {} with matching area
+                        //         let correspondingForecast = forecast.filter(
+                        //             (forecastData: any) =>
+                        //                 forecastData.area === area_metadata[i].name
+                        //         ); //[{area:"AMK", forcast:"windy"}]
+                        //         //have a combined {} of name, label_location, forecast
+                        //         let areaNforecast = {
+                        //             ...area_metadata[i],
+                        //             forecast: correspondingForecast[0].forecast,
+                        //         };
+                        //         areaNforecastArray.push(areaNforecast);
+                        //     }
+                        //     setWeatherData(areaNforecastArray);
                     })
                 )
                 .catch(
@@ -61,15 +62,10 @@ function App() {
     return (
         <div className="container">
             <Container>
-                /*
                 <Header />
-                */ //
-                <DateTimePicker />
-                //
+                  {/* <DateTimePicker />
                 <LocationList />
-                /*
-                <ImageContainer />
-                */
+                <ImageContainer />  */}
             </Container>
         </div>
     );
